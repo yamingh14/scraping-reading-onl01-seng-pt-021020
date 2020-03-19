1936d2facd141bc65270b2bf9e7987d7338e39b8
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-19 06:02:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,20 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+class Course
+  attr_accessor :title, :schedule, :description
+
+  @@all = []
+
+  def initialize
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+end
